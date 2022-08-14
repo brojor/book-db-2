e268a3974ab8e04c76366da6c21828020b15d730
brojor <bronislav.jordan@gmail.com>
2022-08-14 12:42:15 +0200

    fix: es-lint errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/enter',
-      name: 'enter',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/EnterView.vue'),
+      path: "/enter",
+      name: "enter",
+      component: () => import("../views/EnterView.vue"),
     },
   ],
 });
