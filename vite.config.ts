757d0d5d7e5494f5ac33483beff5ff4371dda5b2
brojor <bronislav.jordan@gmail.com>
2022-08-15 10:22:23 +0200

    chore: enable Attributify Mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Unocss from "unocss/vite";
+import presetAttributify from "@unocss/preset-attributify";
+import presetUno from "@unocss/preset-uno";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetAttributify(), presetUno()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
